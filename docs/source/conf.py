cc18c689283e2434f19b6edb6fc18860a96322d2
Aditya V <121498838+Aditya-Venkat@users.noreply.github.com>
2026-05-30 02:07:46 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,7 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'spDDB'
 
 release = '0.1'
 version = '0.1.0'
@@ -17,6 +15,10 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    "myst_nb"
+]
+
+nb_execution_mode = "off"
 ]
 
 intersphinx_mapping = {
